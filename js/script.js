6a5db20b10b7869ec2cd79d3642c3fda6de64ca6
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-07 23:53:29 +0100

    fix*

--- js/script.js
+++ js/script.js
@@ -4,32 +4,32 @@ const members = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        img: 'img/wayne-barnett-founder-ceo.jpg'
+        img: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        img: 'img/angela-caroll-chief-editor.jpg'
+        img: 'angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        img: 'img/walter-gordon-office-manager.jpg'
+        img: 'walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        img: 'img/angela-lopez-social-media-manager.jpg'
+        img: 'angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        img: 'img/scott-estrada-developer.jpg'
+        img: 'scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        img: 'img/barbara-ramos-graphic-designer.jpg'
+        img: 'barbara-ramos-graphic-designer.jpg'
     },
 ]
 
@@ -40,7 +40,7 @@ cardCreator();
 const btnAdd = document.querySelector('#addMemberButton');
 
 btnAdd.addEventListener('click', function() {
-    teamContainer.innerHTML = " ";
+   
     const inputName = document.querySelector('#name');
     const inputRole = document.querySelector('#role');
     const inputImage = document.querySelector('#image');
@@ -67,27 +67,31 @@ btnAdd.addEventListener('click', function() {
 
 // Loop per creare cards
 function cardCreator() {
+    teamContainer.innerHTML = ' '
     for (i = 0; i < members.length; i++) {
 
 
         let div = document.createElement("div");
         div.classList.add("team-card");
-        let img = document.createElement('img');
+       
+        const imgHTML = members[i].img;
+        const roleHTML = members[i].role;
+        const nameHTML = members[i].name;
+
 
         div.innerHTML = `
-        <div class="card-image">
-            <img
-                src="${members[i].img}"
-                alt="${members[i].name}"
-            />
-        </div>
-        <div class="card-text">
-            <h3>${members[i].name}</h3>
-            <p>${members[i].role}</p>
-        </div>`;
+                        <div class="card-image">
+                            <img
+                                src="img/${imgHTML}"
+                                alt="${nameHTML}"
+                            />
+                        </div>
+                        <div class="card-text">
+                            <h3>${nameHTML}</h3>
+                            <p>${roleHTML}</p>
+                        </div>
+                        `;
         
-        // let div = document.createElement("div");
-        // div.classList.add('card-text');
         teamContainer.append(div);
 
     }
